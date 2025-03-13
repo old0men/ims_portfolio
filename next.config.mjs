3254e4152b7f62a7239e48042fea2beb3a1d540f
Aufgabe 1 Victor <pesciv@BZZ.ch>
2025-03-13 10:42:19 +0100

    wasm config taken out of nextconfig file

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.experiments = { asyncWebAssembly: true };
-        return config
-    }
+
 };
 
 export default nextConfig;
