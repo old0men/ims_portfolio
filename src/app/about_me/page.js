e9e59d1bca4fb80637a6ea40cd2a840fb8141687
Aufgabe 1 Victor <pesciv@BZZ.ch>
2025-05-22 08:33:29 +0200

    current working / pre_wasm maybe?

--- src/app/about_me/page.js
+++ src/app/about_me/page.js
@@ -1,6 +1,64 @@
+"use client";
+import { useRef } from "react"
 export default function about() {
+    const squareRef = useRef([])
+
+    const handleClick = (index, event) => {
+
+        event.stopPropagation()
+        let current_square =  squareRef.current[index]
+
+        for (let i = 0; i < squareRef.current.length; i++) {
+            if (squareRef.current[i]?.classList.contains("square_show")) {
+                squareRef.current[i].classList.remove("square_show")
+                console.log("removed")
+            }
+        }
+
+        if (current_square && index !== 5) {
+            console.log(current_square + " : " + squareRef.current)
+            current_square.classList.add("square_show")
+
+        }
+    }
+
+    const handleRemove = () => {
+        for (let i = 0; i < squareRef.current.length; i++) {
+            if (squareRef.current[i]?.classList.contains("square_show")) {
+                squareRef.current[i].classList.remove("square_show")
+                console.log("removed")
+            }
+        }
+    }
+
     return (
-        <h1> world </h1>
+        <div className={"about_me_body"} onClick={() => handleRemove()}>
+            <span>
+
+            </span>
+            <div className={"about_me_flexing_container"}>
+                <article className={"left"}>
+                    <p className={"square"}
+                       ref={(current_square) => (squareRef.current[0] = current_square)}
+                       onClick={(e) => handleClick(0, e)}
+                    ></p>
+                    <p className={"square"}
+                       ref={(current_square) => (squareRef.current[1] = current_square)}
+                       onClick={(e) => handleClick(1, e)}
+                    ></p>
+                </article>
+                <article className={"right"}>
+                    <p className={"square"}
+                       ref={(current_square) => (squareRef.current[2] = current_square)}
+                       onClick={(e) => handleClick(2, e)}
+                    ></p>
+                    <p className={"square"}
+                       ref={(current_square) => (squareRef.current[3] = current_square)}
+                       onClick={(e) => handleClick(3, e)}
+                    ></p>
+                </article>
+            </div>
+        </div>
     )
 
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,19 @@
-import Links from "next/link"
+"use client";
 import "./globals.css"
 import Link from "next/link";
 
 
 export default function Home() {
+
   return (
       <div className={"home_page"}>
           <h1>victor</h1>
           <h1>Pesci</h1>
 
           <b>
-              <Link href="/about_me" className={"button"}>Go to About</Link> <br />
-              <Link href="/project_list" className={"button"}>View Projects</Link>
+              <Link href="./about_me" className={"button"}>Go to About</Link> <br/>
+              <Link href="./project_list" className={"button"}>View Projects</Link>
           </b>
-
       </div>
   );
 }
